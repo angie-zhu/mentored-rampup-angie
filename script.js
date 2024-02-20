cbb2116aa72df57984e2bf3436a8b0442be54e7b
Angie <angiezhu05@gmail.com>
2024-02-19 19:24:04 -0800

    moved files

--- script.js
+++ script.js
@@ -9,3 +9,48 @@ function submit() {
     alert('Username:', username.value, 'Email:', email.value, 'Password:', password.value);
     return false;
 }
+
+function call_button() {
+    
+}
+
+const theDiv = document.querySelector("#link") 
+
+function correct() {
+  alert("Correct! You're one step closer to getting to know me better 🥰🥰")
+}
+
+function wrong() {
+  alert("Wrong😥 please try again.")
+}
+
+
+theDiv.addEventListener('DOMContentLoaded', function() {
+  console.log('pic was clicked');
+});
+
+
+
+
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide( '#image-carousel' ).mount();
+} );
+
+function openPopup() {
+  document.getElementById("popupContainer").style.display = "block";
+}
+
+// Function to close the pop-up
+function closePopup() {
+  document.getElementById("popupContainer").style.display = "none";
+}
+
+function myFunctionOpen() {
+  var popup = document.getElementById("myPopup");
+  popup.style.display = 'block'
+}
+
+function myFunctionOpen() {
+  var popup = document.getElementById("mypop");
+  popup.classList.toggle("show");
+}
